0c10a13f7ede7353b6361b51fbdbc2c97516060e
Adrian609 <rodriguez.adrian609@gmail.com>
2017-01-18 17:29:52 -0500

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,175 @@
+package sample;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable{
+
+    @FXML
+    private AnchorPane beerTab;
+
+    @FXML
+    private Button button1;
+
+    @FXML
+    private Button button2;
+
+    @FXML
+    private Button button3;
+
+    @FXML
+    private Button button4;
+
+    @FXML
+    private Button button5;
+
+    @FXML
+    private Button button6;
+
+    @FXML
+    private Button button8;
+
+    @FXML
+    private Button button7;
+
+    @FXML
+    private Button button9;
+
+    @FXML
+    private Button button10;
+
+    @FXML
+    private Button button11;
+
+    @FXML
+    private Button button12;
+
+    @FXML
+    private Button button13;
+
+    @FXML
+    private Button button14;
+
+    @FXML
+    private Button button15;
+
+    @FXML
+    private Button button16;
+
+    @FXML
+    private Button button17;
+
+    @FXML
+    private Button button18;
+
+    @FXML
+    private Button button19;
+
+    @FXML
+    private Button button20;
+
+    @FXML
+    private Button button21;
+
+    @FXML
+    private Button button22;
+
+    @FXML
+    private Button button23;
+
+    @FXML
+    private Button button24;
+
+    @FXML
+    private Button button25;
+
+    @FXML
+    private Button button26;
+
+    @FXML
+    private Button button27;
+
+    @FXML
+    private Button button28;
+
+    @FXML
+    private Button button29;
+
+    @FXML
+    private Button button30;
+
+    @FXML
+    private Button button31;
+
+    @FXML
+    private Button button32;
+
+    @FXML
+    private Button button33;
+
+    @FXML
+    private Button button34;
+
+    @FXML
+    private Button button35;
+
+    @FXML
+    private Button button36;
+
+    @FXML
+    private Button button37;
+
+    @FXML
+    private Button button38;
+
+    @FXML
+    private Button button39;
+
+    @FXML
+    private Button button40;
+
+    @FXML
+    private Button button41;
+
+    @FXML
+    private Button button42;
+
+    @FXML
+    private Button button43;
+
+    @FXML
+    private Button button44;
+
+    @FXML
+    private Button button45;
+
+    @FXML
+    private Button button46;
+
+    @FXML
+    private Button button47;
+
+    @FXML
+    private Button button48;
+
+    @FXML
+    private Button newTabButton;
+
+    @FXML
+    private Button checkOutButton;
+
+    @FXML
+    private Button deleteButton;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    Button button = new Button("0");
+
+    }
+}
+
