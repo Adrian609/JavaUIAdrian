c3ba99e48992d3dfef4d46cad43777c5992a264d
Adrian609 <rodriguez.adrian609@gmail.com>
2017-01-18 18:06:10 -0500

    classes

---
+++ src/sample/Beer.java
@@ -0,0 +1,15 @@
+package sample;
+
+/**
+ * Created by A-Rod on 1/18/2017.
+ */
+
+/**
+ * Beer Class:
+ * Beer class will handle the beer names and pricing
+ */
+public class Beer {
+
+
+
+}
---
+++ src/sample/Delete.java
@@ -0,0 +1,7 @@
+package sample;
+
+/**
+ * Created by A-Rod on 1/18/2017.
+ */
+public class Delete {
+}
